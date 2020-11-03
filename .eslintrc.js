36f0e0a9f1793c46698026ef22819e7bfe4f5716
Ellis Huang <gjb25638@gmail.com>
2020-11-03 14:38:52 +0800

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: "babel-eslint"
+  },
+  extends: ["@nuxtjs", "plugin:nuxt/recommended"],
+  // add your custom rules here
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "vue/max-attributes-per-line": "off",
+    "vue/html-self-closing": "off",
+    "vue/mustache-interpolation-spacing": "off",
+    "vue/attributes-order": "off",
+    "vue/no-async-in-computed-properties": "off",
+    "vue/no-side-effects-in-computed-properties": "off",
+    semi: "off",
+    quotes: "off",
+    "vue/html-indent": "off",
+    "vue/html-closing-bracket-newline": "off",
+    "space-before-function-paren": "off",
+    "vue/no-unused-components": "off"
+  }
+};
---
+++ layouts/default.vue
@@ -0,0 +1,44 @@
+<template lang="pug">
+  div(data-app)
+    nuxt
+</template>
+
+<script>
+export default {
+  components: {}
+};
+</script>
+
+<style>
+body {
+  font-size: 14px;
+  font-family: "microsoft yahei";
+}
+body,
+div,
+ul,
+li,
+dl,
+dt,
+dd,
+h2,
+h1,
+h3,
+input {
+  padding: 0;
+  margin: 0;
+}
+a {
+  text-decoration: none;
+  cursor: pointer !important;
+}
+ul,
+li {
+  list-style: none;
+}
+h1,
+h3 {
+  padding: 0;
+  margin: 0;
+}
+</style>
